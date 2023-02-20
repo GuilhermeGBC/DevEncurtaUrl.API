028ccc0e01366361470886e4a22100b2eeeec64c
gbcgu <gbcgu@GUILHERME>
2023-02-20 10:09:26 -0300

    Adicionar arquivos de projeto.

---
+++ DevEncurtaUrl.API/Models/AddOrUpdateShortenedLinkModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEncurtaUrl.API.Models
+{
+    public class AddOrUpdateShortenedLinkModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string DestinationLink { get; set; }
+
+    }
+}
---
+++ DevEncurtaUrl.API/Persistence/DevEncurtaUrlDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEncurtaUrl.API.Persistence
+{
+    public class DevEncurtaUrlDbContext
+    {
+        private int _currentIndex = 1;
+
+        public DevEncurtaUrlDbContext()
+        {
+            Links = new List<Entities.ShortenedCustomLink>();
+        }
+        //Lista de links encurtados
+        public List<Entities.ShortenedCustomLink> Links { get; set; }
+
+        //Banco em memória
+        public void Add(Entities.ShortenedCustomLink link)
+        {
+            link.Id = _currentIndex;
+            _currentIndex++;
+
+            Links.Add(link);
+        }
+    }
+}
